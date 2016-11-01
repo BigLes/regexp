1bdbff06e64dcca5155618ca920361dbc291d960
BigLes <bigles@bigmir.net>
2016-11-01 14:27:02 +0100

    new logs of DB connection

--- utils/database/index.js
+++ utils/database/index.js
@@ -1,25 +1,14 @@
-var mongoose  = require('mongoose');
 var Sequelize = require('sequelize');
 var Models    = require('./models');
 
 module.exports = function() {
-    var mongoConnection;
     var connectionString;
-    if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
-        mongoConnection = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-        process.env.OPENSHIFT_APP_NAME;
-    } else {
-        mongoConnection = "mongodb://localhost:27017/puzzles";
-    }
 
     if (process.env.MYSQLCONNSTR_localdb) {
         process.env.MYSQLCONNSTR_localdb.split(";").forEach(function (item) {
             var string = item.split("=");
             if (string[0] === "Password") {
-                connectionString = "mysql://azure:" + string[1] + "@localhost:54454/localdb";
+                connectionString = "mysql://azure:\"" + string[1] + "\"@localhost:54454/localdb";
             }
         })
     } else {
@@ -30,38 +19,11 @@ module.exports = function() {
 
     var sequelize = new Sequelize(connectionString);
 
-    sequelize.authenticate().then(function(errors) { console.log('ERROR!' + errors) });
+    sequelize.authenticate().then(function (result) {console.log('RESULT ' + result)});
 
     var models = new Models(sequelize);
 
     return {
-        saveMongoose: function (model, query, callback) {
-            var item = new model(query);
-            mongoose.connect(mongoConnection);
-            item.save(function (err, doc) {
-                mongoose.connection.close();
-                if (err) {
-                    callback(err, "Not saved");
-                    throw err;
-                } else {
-                    callback(null, "Saved", doc);
-                }
-            });
-        },
-        findMongoose: function (model, query, callback) {
-            mongoose.connect(mongoConnection);
-            model.findOne(query, {_id: 0, __v: 0, strings: 0}, function (err, doc) {
-                mongoose.connection.close();
-                callback(err, doc);
-            })
-        },
-        findAllMongoose: function (model, query, callback) {
-            mongoose.connect(mongoConnection);
-            model.find(query, {_id: 0, count: 0, strings: 0, rules: 0, __v: 0}, function (err, doc) {
-                mongoose.connection.close();
-                callback(err, doc);
-            })
-        },
         save: function (query, callback) {
             return models.puzzle.create(query)
                 .then(function (record) {callback(null, "Saved", record)})
