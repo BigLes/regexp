454f9c7dfe350d136b34ab354c11248eef3dc779
BigLes <bigles@bigmir.net>
2016-11-03 23:22:47 +0100

    update puzzle

--- utils/database/index.js
+++ utils/database/index.js
@@ -25,10 +25,9 @@ module.exports = function() {
     var models = new Models(sequelize);
 
     models.puzzle.findOne({where: {id: 4}}).then(function (puzzle) {
+        puzzle.update({rules: {"x": ["/[^X]*(DN|TE|NI)/g", "/[RONMHC]*I[RONMHC]*/g", "/.*(..)\\1P/g", "/(E|RC|NM)*/g", "/([^MC]|MM|CC)*/g", "/R?(CR)*MC[MA]*/g", "/.*/g", "/.*CDD.*RRP.*/g", "/(XHH|[^XH])*/g", "/([^CME]|ME)*/g", "/.*RXO.*/g", "/.*LR.*RL.*/g", "/.*EU.*ES.*/g"], "y": ["/.*H.*H.*/g", "/(DI|NS|TH|OM)*/g", "/F.*[AO].*[AO].*/g", "/(O|RHH|MM)*/g", "/.*/g", "/C*MC(CCC|MM)*/g", "/[^C]*[^R]*III.*/g", "/(...?)\\1*/g", "/([^X]|XCC)*/g", "/(RR|HHH)*.?/g", "/N.*X.X.X.*E/g", "/R*D*M*/g", "/.(C|HH)*/g"], "z": ["/.*H.*V.*G.*/g", "/[RC]*/g", "/M*XEX.*/g", "/.*MCC.*DD.*/g", "/.*X.*RCHX.*/g", "/.*(.)(.)(.)(.)\\4\\3\\2\\1.*/g", "/(NI|ES|IH).*/g", "/[^C]*MMM[^C]*/g", "/.*(.)X\\1C\\1.*/g", "/[ROMEA]*HO[UMIEC]*/g", "/(XR|[^R])*/g", "/[^M]*M[^M]*/g", "/(S|MM|HHH)*/g"]}})
         console.log(puzzle);
     });
-
-    models.puzzle.destroy({where: {id: {$in: [6,7,8,9,10]}}});
 
     return {
         save: function (query, callback) {
